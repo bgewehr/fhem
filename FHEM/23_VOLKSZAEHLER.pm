872bdcdf79a37e282a3aa2362a3355445b5fa23a
root <root@raspberrypi.(none)>
2015-02-15 23:02:25 +0100

    changes for local HTTP support

--- FHEM/23_VOLKSZAEHLER.pm
+++ FHEM/23_VOLKSZAEHLER.pm
@@ -20,6 +20,10 @@
 #  GNU General Public License for more details.
 #
 #  This copyright notice MUST APPEAR in all copies of the script!
+#  
+#  Changes:
+#  14.02.15 optimizer     enable to query local vzlogger http daemon or middleware daemon
+#
 #
 ################################################################
 # $Id:$
@@ -33,6 +37,7 @@ use Time::Piece;
 use Data::Dumper;
 use LWP::UserAgent;
 use HTTP::Request;
+use Data::Dumper;
 
 sub
 VOLKSZAEHLER_Initialize($)
@@ -41,7 +46,7 @@ VOLKSZAEHLER_Initialize($)
 
   $hash->{DefFn}     = "VOLKSZAEHLER_Define";
   $hash->{AttrList}  = "delay loglevel:0,1,2,3,4,5,6 ".
-    "stateS ".
+    "stateS disable:0,1 ".
     $readingFnAttributes;
 }
 
@@ -55,17 +60,37 @@ VOLKSZAEHLER_Define($$)
   my $host = $a[2]||"";
   my $host_port = $a[3]||"";
   my $channel = $a[4]||"";
-  my $reading = $a[5]||"";
-  my $delay = $a[6]||"";
-  
+  my $reading;
+  my $delay;
+  my $type;
+
+#  return "Wrong syntax: use define <name> VOLKSZAEHLER <ip-address> <port-nr> <channel> <Wert:last/min/max/average/consumption> <poll-delay>" if(int(@a) != 7);
+#  return "Wrong syntax: use define <name> VOLKSZAEHLER <ip-address> <port-nr> <channel> <type:middl/local> <buffer:-x (Anzahl tuples)/x (in Sekunden)> <poll-delay>" if(int(@a) != 7);
+# Allgemeine Lösung mit Erweiterung Parameteranzahl: 
+  # return "Wrong syntax: use define <name> VOLKSZAEHLER <ip-address> <port-nr> <channel> <Wert:last/min/max/average/consumption> <poll-delay> <type:middl/local>" if(int(@a) != 8);
+   
+# zweite Lösung mit gleicher Parameteranzahl:
+    if ( (int(@a) == 7 ) && ( $a[6] ne "local" )) {
+       # "defined for querying Volkszähler middleware";
+       $reading = $a[5]||"";
+       $delay = $a[6]||"";
+    }
+    elsif ( (int(@a) == 7 ) && ( $a[6] eq "local" )) {
+       # "defined for querying local vzlogger http-daemon";
+       $delay = $a[5]||"";
+       $type = $a[6]||"";
+    }  
+    else {
+       return "Wrong syntax: use define <name> VOLKSZAEHLER <ip-address> <port-nr> <channel> <Wert:last/min/max/average/consumption> <poll-delay> or 
+       use define <name> VOLKSZAEHLER <ip-address> <port-nr> <channel> <poll-delay> <local>";
+    }
   $attr{$name}{delay}=$delay if $delay;
-
-  return "Wrong syntax: use define <name> VOLKSZAEHLER <ip-address> <port-nr> <channel> <Wert:last/min/max/average/consumption> <poll-delay>" if(int(@a) != 7);
-
+  
   $hash->{Host} = $host;
   $hash->{Host_Port} = $host_port; 
   $hash->{Channel} = $channel;
   $hash->{Reading} = $reading;
+  $hash->{Type} = $type;
  
   InternalTimer(gettimeofday(), "VOLKSZAEHLER_GetStatus", $hash, 0);
  
@@ -84,6 +109,8 @@ VOLKSZAEHLER_GetStatus($)
   my $host = $hash->{Host}||"";
   my $channel = $hash->{Channel}||"";
   my $reading = $hash->{Reading}||"";
+  my $type = $hash->{Type}||"";
+  my $URL="";
   
   my $delay=$attr{$name}{delay}||300;
   
@@ -92,7 +119,15 @@ VOLKSZAEHLER_GetStatus($)
   if(!defined($hash->{Host_Port})) { return(""); }
   
   my $host_port = $hash->{Host_Port}||"";
-  my $URL="http://".$host.":".$host_port."/middleware.php/data/".$channel.".json?from=".$delay."%20seconds%20ago&tuples=1";
+  if ($type eq "local") { 
+      $URL="http://".$host.":".$host_port."/".$channel;
+      #evtl.auch buffer übergeben?
+      #z.B. buffer = 100 : Die letzten 100 Sekunden übergeben. 
+      #     buffer = -1 : Den letzten Wert/tuples übergeben.
+  }
+  else {
+      $URL="http://".$host.":".$host_port."/middleware.php/data/".$channel.".json?from=".$delay."%20seconds%20ago&tuples=1";
+  } 
   my $agent = LWP::UserAgent->new(env_proxy => 1,keep_alive => 1, timeout => 25)||"";
   my $header = HTTP::Request->new(GET => $URL)||"";
   my $request = HTTP::Request->new('GET', $URL, $header)||"";
@@ -110,28 +145,49 @@ VOLKSZAEHLER_GetStatus($)
   my $decoded = decode_json( $response->content );
   
   #used for debugging
-  #print $response->content."\n";
+  print "debug Info VOLKSZAEHLER content";
+  print $response->content."\n";
   
-  my $min = $decoded->{data}->{min}[1]||"";  
-  my $min_at = $decoded->{data}->{min}[0]||0;
-  $min_at = localtime($min_at/1000);
-  my $max = $decoded->{data}->{max}[1]||"";  
-  my $max_at = $decoded->{data}->{max}[0]||0;  
-  $max_at = localtime($max_at/1000);
-  my $average = $decoded->{data}->{average}||"";
-  my $consumption = $decoded->{data}->{consumption}||"";
-  my $from = $decoded->{data}->{from}||0;
-  $from = localtime($from/1000);
-  my $to = $decoded->{data}->{to}||0;
-  $to = localtime($to/1000);
-  my $last = $decoded->{data}->{tuples}[0][1]||"";
-  my $last_at = $decoded->{data}->{tuples}[0][0]||0;
-  $last_at = localtime($last_at/1000);
-  my $state=$last||"";
+  my ($average, $min, $max, $consumption, $state, $min_at, $max_at, $from, $to, $last, $last_at);
+  
+  if ($type eq "local") {
+  #{ "version": "0.4.0", "generator": "vzlogger", "data": [ { "uuid": "180", "last": 1423958150541, "interval": 30, "protocol": "d0", "tuples": [ [ 1423958150541, 15094.700000 ] ] } ] }
+      # foreach my $k (keys $decoded) {
+         # print "$k: " . $decoded->{$k} . "\n";
+      # }
+     print Dumper(\%$decoded); #only with: use Data::Dumper;
+     $last = $decoded->{data}->{tuples}[0][1]||"";
+#     $last = $$decoded{data}{tuples}[0][1]||"";
+#     print "\n last = $last \n";
+     $last_at = $decoded->{data}->{tuples}[0][0]||0;
+#     $last_at = $$decoded{data}{tuples}[0][0]||0;
+     $last_at = localtime($last_at/1000);
+#     print "last_at =  $last_at \n";
+     $state=$last||"";
+  }
+  else {
+  #{"version":"0.3","data":{"uuid":"abc","from":1403006628278,"to":1403006750232,"min":[1403006750232,8278.1],"max":[1403006689218,8341.319],"average":8309.691,"consumption":281.5,"rows":3,"tuples":[[1403006689218,8341.319,1],[1403006750232,8278.1,1]]}}
+     $min = $decoded->{data}->{min}[1]||"";  
+     $min_at = $decoded->{data}->{min}[0]||0;
+     $min_at = localtime($min_at/1000);
+     $max = $decoded->{data}->{max}[1]||"";  
+     $max_at = $decoded->{data}->{max}[0]||0;  
+     $max_at = localtime($max_at/1000);
+     $average = $decoded->{data}->{average}||"";
+     $consumption = $decoded->{data}->{consumption}||"";
+     $from = $decoded->{data}->{from}||0;
+     $from = localtime($from/1000);
+     $to = $decoded->{data}->{to}||0;
+     $to = localtime($to/1000);
+     $last = $decoded->{data}->{tuples}[0][1]||"";
+     $last_at = $decoded->{data}->{tuples}[0][0]||0;
+     $last_at = localtime($last_at/1000);
+     $state=$last||"";
+  }
   
   SELECT:{
   if ($reading eq "average"){$state = $average; last SELECT; }
-  if ($reading eq "min"){$state = $min; last SELECT; }
+  if ($reading eq "min"){$state = $min; last SELECT; } 
   if ($reading eq "max"){$state = $max; last SELECT; }
   if ($reading eq "consumption"){$state = $consumption; last SELECT; }
   }
