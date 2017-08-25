f43a55b1fd1b7b7988c2cf0eb5c900fb61d52909
Bernd Gewehr <bgewehr@users.noreply.github.com>
2017-08-25 06:37:19 +0200

    Update 23_VOLKSZAEHLER.pm
    Updated code from local installation

--- FHEM/23_VOLKSZAEHLER.pm
+++ FHEM/23_VOLKSZAEHLER.pm
@@ -40,12 +40,11 @@ VOLKSZAEHLER_Initialize($)
   my ($hash) = @_;
 
   $hash->{DefFn}     = "VOLKSZAEHLER_Define";
-  $hash->{AttrList}  = "delay loglevel:0,1,2,3,4,5,6 ".
+  $hash->{AttrList}  = "delay period loglevel:0,1,2,3,4,5,6 ".
     "stateS ".
     $readingFnAttributes;
 }
 
-sub
 VOLKSZAEHLER_Define($$)
 {
   my ($hash, $def) = @_;
@@ -55,20 +54,24 @@ VOLKSZAEHLER_Define($$)
   my $host = $a[2]||"";
   my $host_port = $a[3]||"";
   my $channel = $a[4]||"";
-  my $reading = $a[5]||"";
+  my $reading = uc($a[5])||"";
   my $delay = $a[6]||"";
-  
-  $attr{$name}{delay}=$delay if $delay;
+  my $period = $a[7]||$delay;
 
-  return "Wrong syntax: use define <name> VOLKSZAEHLER <ip-address> <port-nr> <channel> <Wert:last/min/max/average/consumption> <poll-delay>" if(int(@a) != 7);
+  $attr{$name}{delay}=$delay if $delay;
+  $attr{$name}{period}=$period if $period;
+  $attr{$name}{'event-on-change-reading'} = uc($reading);
+  $attr{$name}{stateFormat} = uc($reading);
+
+  return "Wrong syntax: use define <name> VOLKSZAEHLER <ip-address> <port-nr> <channel> <Wert:last/min/max/average/consumption> <poll-delay> optional: <period>" if(int(@a) < 7);
 
   $hash->{Host} = $host;
-  $hash->{Host_Port} = $host_port; 
+  $hash->{Host_Port} = $host_port;
   $hash->{Channel} = $channel;
   $hash->{Reading} = $reading;
- 
+
   InternalTimer(gettimeofday(), "VOLKSZAEHLER_GetStatus", $hash, 0);
- 
+
   return undef;
 }
 
@@ -84,15 +87,17 @@ VOLKSZAEHLER_GetStatus($)
   my $host = $hash->{Host}||"";
   my $channel = $hash->{Channel}||"";
   my $reading = $hash->{Reading}||"";
-  
+
   my $delay=$attr{$name}{delay}||300;
-  
+  my $period=$attr{$name}{period}||$delay;
+
+  #Log 0, $name.' Delay: '.$delay.' Period: '.$period;
   InternalTimer(gettimeofday()+$delay, "VOLKSZAEHLER_GetStatus", $hash, 0);
 
   if(!defined($hash->{Host_Port})) { return(""); }
-  
+
   my $host_port = $hash->{Host_Port}||"";
-  my $URL="http://".$host.":".$host_port."/middleware.php/data/".$channel.".json?from=".$delay."%20seconds%20ago&tuples=1";
+  my $URL="http://".$host.":".$host_port."/middleware.php/data/".$channel.".json?from=".$period."%20seconds%20ago&tuples=1";
   my $agent = LWP::UserAgent->new(env_proxy => 1,keep_alive => 1, timeout => 25)||"";
   my $header = HTTP::Request->new(GET => $URL)||"";
   my $request = HTTP::Request->new('GET', $URL, $header)||"";
@@ -108,71 +113,105 @@ VOLKSZAEHLER_GetStatus($)
   }
 
   my $decoded = decode_json( $response->content );
-  
+
   #used for debugging
   #print $response->content."\n";
-  
-  my $min = $decoded->{data}->{min}[1]||"";  
+  #print Dumper($decoded);
+
+  # my $count = $decoded->{data}->{rows}||0;
+  # print $count, "\n";
+
+  # $count = $count - 2;
+  # print $count, "\n";
+
+  if ($decoded->{data}) {
+
+  my $min = $decoded->{data}->{min}[1]||0;
   my $min_at = $decoded->{data}->{min}[0]||0;
   $min_at = localtime($min_at/1000);
-  my $max = $decoded->{data}->{max}[1]||"";  
-  my $max_at = $decoded->{data}->{max}[0]||0;  
+  my $max = $decoded->{data}->{max}[1]||0;
+  my $max_at = $decoded->{data}->{max}[0]||0;
   $max_at = localtime($max_at/1000);
-  my $average = $decoded->{data}->{average}||"";
-  my $consumption = $decoded->{data}->{consumption}||"";
+  my $average = $decoded->{data}->{average}||0;
+  my $consumption = $decoded->{data}->{consumption}||0;
   my $from = $decoded->{data}->{from}||0;
   $from = localtime($from/1000);
   my $to = $decoded->{data}->{to}||0;
   $to = localtime($to/1000);
-  my $last = $decoded->{data}->{tuples}[0][1]||"";
-  my $last_at = $decoded->{data}->{tuples}[0][0]||0;
-  $last_at = localtime($last_at/1000);
-  my $state=$last||"";
-  
+  Log 5, "VOLKSZAEHLER_debug: $name $host_port ".$hash->{STATE}." -> ".$response->content."\n";
+  my $last = 0;
+  my $last_at = $to;
+  my $state = 0;
+
+  my @tuples =  $decoded->{data}->{tuples};
+  my $tuplescount = @tuples;
+
+  Log 5, "VOLKSZAEHLER_debug: $tuplescount @tuples"."\n";
+
+  if  ($tuplescount > 0 && $decoded->{data}->{rows} > 0){
+      $last = $decoded->{data}->{tuples}[-1][1]||0;
+      $last_at = $decoded->{data}->{tuples}[-1][0]||0;
+      $last_at = localtime($last_at/1000);
+      $state = $last||0;
+  };
+
   SELECT:{
   if ($reading eq "average"){$state = $average; last SELECT; }
   if ($reading eq "min"){$state = $min; last SELECT; }
   if ($reading eq "max"){$state = $max; last SELECT; }
   if ($reading eq "consumption"){$state = $consumption; last SELECT; }
   }
-  
-  
+
   Log 4, "VOLKSZAEHLER_GetStatus: $name $host_port ".$hash->{STATE}." -> ".$state;
 
-  my $text=$reading.": ".$state||"";
-  
-  $hash->{STATE} = substr($reading,0,1).": ".$state;
-  $hash->{CHANGED}[0] = $text;
-  
-  my $sensor0="CONSUMPTION";
-  $hash->{READINGS}{$sensor0}{TIME} = TimeNow();
-  $hash->{READINGS}{$sensor0}{VAL} = $consumption;
-   
-  my $sensor1="MIN";
-  $hash->{READINGS}{$sensor1}{TIME} = $min_at->strftime('%Y-%m-%d %H:%M:%S');
-  $hash->{READINGS}{$sensor1}{VAL} = $min;
-  
-  my $sensor2="MAX";
-  $hash->{READINGS}{$sensor2}{TIME} = $max_at->strftime('%Y-%m-%d %H:%M:%S');
-  $hash->{READINGS}{$sensor2}{VAL} = $max;
-  
-  my $sensor3="AVERAGE";
-  $hash->{READINGS}{$sensor3}{TIME} = TimeNow();
-  $hash->{READINGS}{$sensor3}{VAL} = $average;
-  
-  my $sensor4="LAST";
-  $hash->{READINGS}{$sensor4}{TIME} = $last_at->strftime('%Y-%m-%d %H:%M:%S');
-  $hash->{READINGS}{$sensor4}{VAL} = $last;
-    
-  my $sensor5="FROM";
-  $hash->{READINGS}{$sensor5}{TIME} = $from->strftime('%Y-%m-%d %H:%M:%S');
-  $hash->{READINGS}{$sensor5}{VAL} = "";
-  
-  my $sensor6="TO";
-  $hash->{READINGS}{$sensor6}{TIME} = $to->strftime('%Y-%m-%d %H:%M:%S');
-  $hash->{READINGS}{$sensor6}{VAL} = "";
-      
-  DoTrigger($name, undef) if($init_done);
-}
+  my $i;
+  my $ts;
 
+  readingsBeginUpdate($hash);
+  $ts = localtime()->strftime('%Y-%m-%d %H:%M:%S');
+  $hash->{".updateTimestamp"} = $ts;
+
+  readingsBulkUpdate($hash, "CONSUMPTION", $consumption );
+
+  $i = $#{ $hash->{CHANGED} };
+  $ts = $min_at->strftime('%Y-%m-%d %H:%M:%S');
+  $hash->{".updateTimestamp"} = $ts;
+  readingsBulkUpdate($hash, "MIN", $min );
+  $hash->{CHANGETIME}->[$#{ $hash->{CHANGED} }] = $ts if ($#{ $hash->{CHANGED} } != $i ); # only add ts if there is a event to
+
+  readingsBulkUpdate($hash, "MIN_AT", $min_at->strftime('%Y-%m-%d %H:%M:%S'));
+
+  $i = $#{ $hash->{CHANGED} };
+  $ts = $max_at->strftime('%Y-%m-%d %H:%M:%S');
+  $hash->{".updateTimestamp"} = $ts;
+  readingsBulkUpdate($hash, "MAX", $max );
+  $hash->{CHANGETIME}->[$#{ $hash->{CHANGED} }] = $ts if ($#{ $hash->{CHANGED} } != $i ); # only add ts if there is a event to
+
+  readingsBulkUpdate($hash, "MAX_AT", $max_at->strftime('%Y-%m-%d %H:%M:%S'));
+
+  readingsBulkUpdate($hash, "AVERAGE", $average );
+
+  $i = $#{ $hash->{CHANGED} };
+  $ts = $last_at->strftime('%Y-%m-%d %H:%M:%S');
+  $hash->{".updateTimestamp"} = $ts;
+  readingsBulkUpdate($hash, "LAST", $last );
+  $hash->{CHANGETIME}->[$#{ $hash->{CHANGED} }] = $ts if ($#{ $hash->{CHANGED} } != $i ); # only add ts if there is a event to
+
+  readingsBulkUpdate($hash, "LAST_AT", $last_at->strftime('%Y-%m-%d %H:%M:%S'));
+
+  $i = $#{ $hash->{CHANGED} };
+  $ts = $from->strftime('%Y-%m-%d %H:%M:%S');
+  $hash->{".updateTimestamp"} = $ts;
+  readingsBulkUpdate($hash, "FROM", $from->strftime('%Y-%m-%d %H:%M:%S'));
+  $hash->{CHANGETIME}->[$#{ $hash->{CHANGED} }] = $ts if ($#{ $hash->{CHANGED} } != $i ); # only add ts if there is a event to
+
+  $i = $#{ $hash->{CHANGED} };
+  $ts = $to->strftime('%Y-%m-%d %H:%M:%S');
+  $hash->{".updateTimestamp"} = $ts;
+  readingsBulkUpdate($hash, "TO", $to->strftime('%Y-%m-%d %H:%M:%S'));
+  $hash->{CHANGETIME}->[$#{ $hash->{CHANGED} }] = $ts if ($#{ $hash->{CHANGED} } != $i ); # only add ts if there is a event to
+
+  readingsEndUpdate($hash, 1);
+}
+}
 1;
